ee75c2f281bc55d74cff5de029642674cd4e7019
Carlos Orellana <ce.orellanaf@gmail.com>
2018-07-17 11:55:51 -0500

    Creado codigo para tomar links de papers por año, tomados links de 2010 a 2013

--- paperLinks.py
+++ paperLinks.py
@@ -6,40 +6,20 @@ from selenium.webdriver.common.keys import Keys
 import time
 import lxml.html as LH
 import lxml.html.clean as clean
+import nltk
+import os
 
-# class paperItem(Item):
-#     # define the fields for your item here like:
-#     title = Field()
-#     price = Field()
-#     #url = scrapy.Field()
-#     description = Field()
-#     address = Field()
-
-# driver = webdriver.Chrome("C:\\Users\\Carlo Orellana\\Downloads\\chromedriver.exe")
-# driver.get('https://scholar.google.com/scholar?hl=es&as_sdt=0%2C5&q=programming+language&btnG=&oq=programming+')
-# time.sleep(1)
-
-# r = requests.get("https://scholar.google.com/scholar?q=programming+language&hl=es&num=20&as_sdt=0&as_vis=1")
-# soup  = BeautifulSoup(r.content)
-# text = soup.get_text()
-# print (text)
-
-# elem = driver.page_source
-# time.sleep(0.2)
-# post_elems = driver.page_source
-# soup = BeautifulSoup(post_elems)
-# print(soup.prettify())
-
-def getWords(url,browser):
+def getWordsHtml(url,archivo):
+    browser = webdriver.Chrome("C:\\Users\\Carlo Orellana\\Downloads\\chromedriver.exe")
     ignore_tags = ('script', 'noscript', 'style')
     browser.get(url)  # Load page
     content = browser.page_source
     cleaner = clean.Cleaner()
     content = cleaner.clean_html(content)
-    with open('source.html', 'w') as f:
-        f.write(content.encode('utf-8'))
+    # open('source.html', 'w') as f:
+    #    f.write(content.encode('utf-8'))
     doc = LH.fromstring(content)
-    with open('result.txt', 'w') as f:
+    with open(archivo, 'w') as f:
         for elt in doc.iterdescendants():
             if elt.tag in ignore_tags: continue
             text = elt.text or ''
@@ -47,22 +27,39 @@ def getWords(url,browser):
             words = ' '.join((text, tail)).strip()
             if words:
                 words = words.encode('utf-8')
-                f.write(words + '\n')
+                f.write(words.decode("utf-8") + '\n')
+    browser.close()
+
+year = '2014'
+if not os.path.exists(year):
+    os.makedirs(year)
 
 url="https://scholar.google.com/scholar?hl=es&as_sdt=0%2C5&q=programming+language&btnG=&oq=programming+"
 browser = webdriver.Chrome("C:\\Users\\Carlo Orellana\\Downloads\\chromedriver.exe")
 browser.get(url)  # Load page
 input("Resuelva los captchas (si los hubiese), deseleccione citas y patentes y presione enter para comenzar ")
 
-
 isPresent = len(browser.find_elements_by_xpath('//*[@id="gs_n"]/center/table/tbody/tr/td[12]/a')) >0
 i = 1
+file = open(year+"/links.txt",'a')
 while (isPresent):
+    j = 1
     paperUrl = browser.find_elements_by_class_name('gs_rt')
     print("Pagina " + str(i))
     for paper in paperUrl:
         link = paper.find_element_by_tag_name('a')
-        print(link.get_attribute('href'))
+        link = link.get_attribute('href')
+        #r = requests.get(link)
+        #content_type = r.headers.get('content-type')
+        #if 'application/pdf' in content_type:
+        #    ext = '.pdf'
+        #if 'text/html' in content_type:
+        #    getWordsHtml(link, year + "/" + str(i) + "-" + str(j) + ".txt")
+        file.write(link + '\n')
+        #else:
+        #    ext = ''
+        #    print('Unknown type: {}'.format(content_type))
+        #time.sleep(1)
     user = browser.find_element_by_xpath('//*[@id="gs_n"]/center/table/tbody/tr/td[12]/a')
     user.click()
     time.sleep(2)
@@ -76,4 +73,5 @@ while (isPresent):
             input("Resuelva el captcha y presione enter ")
             print("Continuando con el programa...")
         else:
+            file.close()
             print("Cerrando el programa...")
